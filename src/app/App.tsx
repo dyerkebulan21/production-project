bb2528ae72447e551ae9e091b580715cf4b344cd
dyerkebulan21 <dyerkebulan21@gmail.com>
2024-05-03 21:10:56 +0500

    add sidebar layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,21 @@
 import "./styles/index.scss";
 
-import { Navbar } from "widgets/Navbar"; 
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 
 export const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-     
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
