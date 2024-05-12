752e831fd54a89fef36db1bea23d6205a1eaf452
dyerkebulan21 <dyerkebulan21@gmail.com>
2024-05-13 00:02:05 +0500

    add pageloader widgets and shared loader component

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,22 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routerConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={<div className="page-wrapper">{element}</div>}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+        <Routes>
+            {Object.values(routeConfig).map(({ element, path }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={
+                        <Suspense fallback={<PageLoader />}>
+                            <div className="page-wrapper">{element}</div>{' '}
+                        </Suspense>
+                    }
+                />
+            ))}
+        </Routes>
     );
 };
 
