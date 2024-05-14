342ec30053790adb627c8efc2ad11bfbea797584
dyerkebulan21 <dyerkebulan21@gmail.com>
2024-05-14 23:45:22 +0500

    add react-testing library and jest-dom

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -74,7 +74,11 @@ const config: Config = {
 
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: [
-    "node_modules"
+    "node_modules", '<rootDir>'
+  ],
+
+  modulePaths: [
+    '<rootDir>src',
   ],
 
   // An array of file extensions your modules use
@@ -88,6 +92,8 @@ const config: Config = {
     "json",
     "node"
   ],
+
+
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -125,6 +131,8 @@ const config: Config = {
   // The root directory that Jest should scan for tests and modules within
   rootDir: '../../',
 
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests .ts'], 
+
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
   //   "<rootDir>"
@@ -155,6 +163,9 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
+
+
+
   testMatch: [
    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
